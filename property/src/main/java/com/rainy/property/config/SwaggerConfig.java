4c7db495ca2834d4cbea51236394243915e23a48
347890208 <49516669+347890208@users.noreply.github.com>
2020-04-06 04:10:46 +0800

    14天不小心回档,哭了

--- property/src/main/java/com/rainy/property/config/SwaggerConfig.java
+++ property/src/main/java/com/rainy/property/config/SwaggerConfig.java
@@ -4,14 +4,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.core.env.Profiles;
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.ArrayList;
+import java.util.List;
+
+import static com.google.common.collect.Lists.newArrayList;
 
 /**
  * 配置swagger
@@ -20,22 +24,56 @@ import java.util.ArrayList;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
+//    @Bean
+//    public Docket docket (Environment environment){
+//        //设置要显示的Swagger环境
+//        Profiles profiles = Profiles.of("application");
+//        //通过boolean确定是否开启Swagger
+//        boolean b = environment.acceptsProfiles(profiles);
+//
+//
+//        return new Docket(DocumentationType.SWAGGER_2)
+//                .apiInfo(apiInfo())
+//                //enable  true可以网页进入Swagger,false相反
+//                .enable(true)
+//                .select()
+//                .apis(RequestHandlerSelectors.basePackage("com.rainy.property.controller"))
+//                .build();
+//
+//    }
     @Bean
-    public Docket docket (Environment environment){
-        //设置要显示的Swagger环境
-        Profiles profiles = Profiles.of("application");
-        //通过boolean确定是否开启Swagger
-        boolean b = environment.acceptsProfiles(profiles);
-
-
-        return new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(apiInfo())
-                //enable  true可以网页进入Swagger,false相反
-                .enable(true)
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2).
+                useDefaultResponseMessages(false)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.rainy.property.controller"))
-                .build();
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.regex("^(?!auth).*$"))
+                .build()
+                .securitySchemes(securitySchemes())
+                .securityContexts(securityContexts())
+                ;
+    }
 
+    private List<ApiKey> securitySchemes() {
+        return newArrayList(
+                new ApiKey("Authorization", "Authorization", "header"));
+    }
+
+    private List<SecurityContext> securityContexts() {
+        return newArrayList(
+                SecurityContext.builder()
+                        .securityReferences(defaultAuth())
+                        .forPaths(PathSelectors.regex("^(?!auth).*$"))
+                        .build()
+        );
+    }
+
+    List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return newArrayList(
+                new SecurityReference("Authorization", authorizationScopes));
     }
 
     private ApiInfo apiInfo(){
---
+++ property/src/main/java/com/rainy/property/config/security/SelfUserDetailsService.java
@@ -0,0 +1,31 @@
+package com.rainy.property.config.security;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Component
+public class SelfUserDetailsService implements UserDetailsService {
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        //构建用户信息的逻辑(取数据库/LDAP等用户信息)
+
+        SelfUserDetails userInfo = new SelfUserDetails();
+        userInfo.setUsername(username);
+        userInfo.setPassword(new BCryptPasswordEncoder().encode("123"));
+
+        Set authoritiesSet = new HashSet();
+        GrantedAuthority authority = new SimpleGrantedAuthority("ROLE_ADMIN");
+        authoritiesSet.add(authority);
+        userInfo.setAuthorities(authoritiesSet);
+
+        return userInfo;
+    }
+}
